2c6e098769722720abc0902781c28ee4190c2b04
mirumi <mail@mirumi.tech>
2022-12-14 18:38:59 +0900

    add: page data and its showing in post page

--- src/pages/[post].vue
+++ src/pages/[post].vue
@@ -1,6 +1,38 @@
 <template>
   <div class="post_view">
-    
+    <div class="header">
+      <div class="title">
+
+      </div>
+      <div class="thumbnail">
+
+      </div>
+      <div class="meta">
+        <div class="category">
+          <span>かてごり: </span>
+          <NuxtLink :to="`/category/${categorySlug}`">{{ categoryName }}</NuxtLink>
+        </div>
+        <div class="created_at">
+          <span>投稿日: </span>
+          <time :datetime="post.date">{{ friendlyDatetime(post.date) }}</time>
+        </div>
+        <div class="updated_at">
+          <span>更新日: </span>
+          <time :datetime="post.modified">{{ friendlyDatetime(post.modified) }}</time>
+        </div>
+        <div class="author">
+          <span>書いた人: </span>
+          <a href="https://twitter.com/milmemo_net" target="_top">@mirumi</a>
+        </div>
+      </div>
+      <div class="share">
+
+      </div>
+    </div>
+    <div class="content" v-html="post.content.rendered"></div>
+    <div class="footer">
+
+    </div>
   </div>
 </template>
 
@@ -17,6 +49,22 @@ const { data: resPosts } = await useFetch(`/posts`, {
 const post = JSON.parse(resPosts.value as string)[0]
 
 console.log(post)
+
+const { data: resCategories } = await useFetch(`/categories/${post.categories[0]}`, {  // absolutely `[0]`
+  baseURL: appConfig.baseURL,
+  params: {
+    _fields: "name,slug",
+  },
+})
+const category: {
+  name: string,
+  slug: string,
+} = JSON.parse(resCategories.value as string)
+const categoryName = category.name
+const categorySlug = category.slug
+
+post.date = post.date + "+09:00"
+post.modified = post.modified + "+09:00"
 
 const { data: resTags } = await useFetch(`/tags`, {
   baseURL: appConfig.baseURL,
@@ -35,19 +83,48 @@ const metaDesc = JSON.parse(resMetaDesc.value as string)
 
 
 
-
 useSetMeta({
   title: post.title.rendered,
   description: metaDesc,
   keywords: tags.join(","),
   url: appConfig.siteFullPath + route.path,
-  createdAt: post.date + "+09:00",
-  updatedAt: post.modified + "+09:00",
+  createdAt: post.date,
+  updatedAt: post.modified,
 })
 </script>
 
 <style lang="scss" scoped>
 .post_view {
-  
+  .header {
+    .title {
+
+    }
+    .thumbnail {
+
+    }
+    .meta {
+      .category {
+
+      }
+      .created_at {
+
+      }
+      .updated_at {
+
+      }
+      .author {
+
+      }
+    }
+    .share {
+
+    }
+  }
+  .content {
+
+  }
+  .footer {
+
+  }
 }
 </style>
