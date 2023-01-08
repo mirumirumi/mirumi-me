7aed4ee01051e8abe3337fe39d640085c0b24a8a
mirumi <mail@mirumi.tech>
2023-01-08 16:50:42 +0900

    add: @vueuse/nuxt for auto import vueuse in Nuxt 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,6 +49,9 @@ export default defineNuxtConfig({
     //   mode: "out-in",
     // },
   },
+  modules: [
+    "@vueuse/nuxt",
+  ],
   nitro: {
     // prerender: {  // for SSR + SSG
     //   routes: []
