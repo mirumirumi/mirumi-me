6a2e11f9894ad21c192ca5260504f6406eec11f2
mirumi <mail@mirumi.tech>
2023-01-22 22:36:12 +0900

    rename: `/page/x` route param name for x

---
+++ src/pages/entries/page/[pageNumber].vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="entries_view_pages">
+    
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+
+</script>
+
+<style lang="scss" scoped>
+.entries_view_pages {
+  
+}
+</style>
