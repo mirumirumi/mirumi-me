e34886764f22453b4eb7696595a474376282fd58
mirumi <mail@mirumi.tech>
2023-01-26 23:01:23 +0900

    feat: top page!

---
+++ src/components/parts/TopPageIndexBlock.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="top_page_index_block">
+    <h3 class="block_title">
+      {{ blockTitle }}
+    </h3>
+    <div class="indexes">
+      <NuxtLink v-for="index in indexes" :to="`/${index.slug}`" class="index" :key="index.slug">
+        <div class="thumbnail">
+          <img :src="index.thumbnailUrl" :alt="index.title" width="412" height="216" />
+        </div>
+        <div class="title">
+          {{ index.title }}
+        </div>
+      </NuxtLink>
+    </div>
+    <div class="link">
+      <NuxtLink :to="linkTo">
+        <span class="text">{{ linkText }}</span><span class="arrow">&nbsp;→</span>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PageSummary } from "@/utils/defines"
+
+defineProps<{
+  blockTitle: string
+  indexes: PageSummary[]
+  linkText: string
+  linkTo: string
+}>()
+</script>
+
+<style lang="scss" scoped>
+.top_page_index_block {
+  .block_title {
+    padding: 1em;
+    color: #a39d98;
+    font-size: 1.13em;
+    line-height: 1;
+    font-weight: bold;
+    text-align: center;
+    user-select: none;
+  }
+  .indexes {
+    display: grid;
+    grid-template-columns: 50% 50%;
+    a.index {
+      display: inline-block;
+      margin: auto 0.3em 0.7em;
+      padding: 0.3em;
+      text-decoration: none;
+      border-radius: 8px;
+      transition: 0.23s all ease;
+      .thumbnail {
+        img {
+          width: 100%;
+          height: auto;
+          border: solid 2.3px #e4dcd2;
+          border-radius: 10px;
+          aspect-ratio: 412 / 216;
+        }
+      }
+      .title {
+        height: 80px;
+        margin: 0.4em;
+        color: #545454;
+        line-height: 1.3;
+        font-size: 0.777em;
+        font-weight: bold;
+      }
+    }
+  }
+  .link {
+    margin-bottom: 4.3em;
+    font-size: 0.93em;
+    font-weight: bold;
+    text-align: center;
+    a {
+      color: var(--color-link);
+      user-select: none;
+      text-decoration: none;
+      .text {
+        text-decoration: underline;
+      }
+      .arrow {
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,305 @@
 <template>
   <div class="index_view">
-    
+    <div class="row">
+      <PartsTopPageIndexBlock
+        :blockTitle="'はじめましての 10 記事'"
+        :indexes="posts.slice(0, 4)"
+        :linkText="'残りの 6 記事'"
+        :linkTo="'/nice-to-meet-you-10'"
+      />
+      <PartsTopPageIndexBlock
+        :blockTitle="'新しい記事'"
+        :indexes="posts.slice(4, 8)"
+        :linkText="'新着記事一覧'"
+        :linkTo="'/entries'"
+      />
+    </div>
+    <div class="row">
+      <PartsTopPageIndexBlock
+        :blockTitle="'PC'"
+        :indexes="posts.slice(8, 12)"
+        :linkText="'PC カテゴリ'"
+        :linkTo="'/category/pc'"
+      />
+      <PartsTopPageIndexBlock
+        :blockTitle="'スマートフォン'"
+        :indexes="posts.slice(12, 16)"
+        :linkText="'スマートフォン カテゴリ'"
+        :linkTo="'/category/mobile'"
+      />
+    </div>
+    <div class="row">
+      <PartsTopPageIndexBlock
+        :blockTitle="'くらし'"
+        :indexes="posts.slice(16, 20)"
+        :linkText="'くらし カテゴリ'"
+        :linkTo="'/category/life'"
+      />
+      <PartsTopPageIndexBlock
+        :blockTitle="'ゲーム'"
+        :indexes="posts.slice(20, 24)"
+        :linkText="'ゲーム カテゴリ'"
+        :linkTo="'/category/game'"
+      />
+    </div>
+    <div class="row">
+      <PartsTopPageIndexBlock
+        :blockTitle="'ブログ'"
+        :indexes="posts.slice(24, 28)"
+        :linkText="'ブログ カテゴリ'"
+        :linkTo="'/category/blog'"
+      />
+      <PartsTopPageIndexBlock
+        :blockTitle="'Software Design'"
+        :indexes="posts.slice(28, 32)"
+        :linkText="'Software Design サマリー記事一覧'"
+        :linkTo="'/category/software-design'"
+      />
+    </div>
+    <div class="row">
+      <PartsTopPageIndexBlock
+        :blockTitle="'明晰夢/体外離脱'"
+        :indexes="posts.slice(32, 34)"
+        :linkText="'明晰夢/体外離脱 …？'"
+        :linkTo="'/category/dreaming'"
+      />
+      <PartsTopPageIndexBlock
+        :blockTitle="'雑記'"
+        :indexes="posts.slice(34)"
+        :linkText="'雑記'"
+        :linkTo="'/category/notes'"
+      />
+    </div>
+    <Teleport to="body">
+      <ClientOnly>
+        <PartsTopButton />
+      </ClientOnly>
+    </Teleport>
   </div>
 </template>
 
 <script setup lang="ts">
-
-
-
-
+import { PageSummary } from "@/utils/defines"
 
 const appConfig = useAppConfig()
+
+let postIds: number[] = []
+
+/**
+ * Prepare nice-to-meet-you-10 entries
+ */
+let posts: PageSummary[] = [
+  {
+    slug: "pc-freesoft",
+    title: "Windows にまず入れるべきおすすめフリーソフト/便利アプリ 40 選",
+    thumbnailUrl: "https://mirumi.me/wp-content/uploads/windows-freesoft-recommend-list-412x216-1.webp",
+  },
+  {
+    slug: "good-goods",
+    title: "これまでの人生で「本当に買ってよかった」と思えるもの 40 選",
+    thumbnailUrl: "https://mirumi.me/wp-content/uploads/good-recommend-goods-milmemo-412x216.webp",
+  },
+  {
+    slug: "out-of-body",
+    title: "この記事で人生変わるかも？体外離脱 (幽体離脱) 総まとめ",
+    thumbnailUrl: "https://mirumi.me/wp-content/uploads/taigai-ridatsu-lucid-dreaming-milmemo-412x216-1.webp",
+  },
+  {
+    slug: "blog-for-you",
+    title: "「みるめも」のような雑記ブログで副業したい人のために頑張って書いた記事",
+    thumbnailUrl: "https://mirumi.me/wp-content/uploads/blog-for-you-only-start-412x216.webp",
+  },
+]
+
+/**
+ * Prepare new entries
+ */
+const { data: resNewEntries } = await useFetch(`/wp/v2/posts`, {
+  baseURL: appConfig.baseURL,
+  params: {
+    // Retrieve only the first 4 (or 2) indexes
+    page: 1,
+    per_page: 4,
+    type: "post",
+    subtype: "post",
+    status: ["publish"],
+    categories_exclude: [1877],  // Software Design
+    _fields: "id",
+  },
+  parseResponse: JSON.parse,
+})
+for (const p of resNewEntries.value as Record<string, number>[]) {
+  postIds.push(p.id)
+}
+
+/**
+ * Prepare "pc" category entries
+ */
+const { data: resPc } = await useFetch(`/wp/v2/posts`, {
+  baseURL: appConfig.baseURL,
+  params: {
+    page: 1,
+    per_page: 4,
+    type: "post",
+    subtype: "post",
+    status: ["publish"],
+    categories: [982],
+    _fields: "id",
+  },
+  parseResponse: JSON.parse,
+})
+for (const p of resPc.value as Record<string, number>[]) {
+  postIds.push(p.id)
+}
+
+/**
+ * Prepare "mobile" category entries
+ */
+const { data: resMobile } = await useFetch(`/wp/v2/posts`, {
+  baseURL: appConfig.baseURL,
+  params: {
+    page: 1,
+    per_page: 4,
+    type: "post",
+    subtype: "post",
+    status: ["publish"],
+    categories: [1010],
+    _fields: "id",
+  },
+  parseResponse: JSON.parse,
+})
+for (const p of resMobile.value as Record<string, number>[]) {
+  postIds.push(p.id)
+}
+
+/**
+ * Prepare "life" category entries
+ */
+const { data: resLife } = await useFetch(`/wp/v2/posts`, {
+  baseURL: appConfig.baseURL,
+  params: {
+    page: 1,
+    per_page: 4,
+    type: "post",
+    subtype: "post",
+    status: ["publish"],
+    categories: [169],
+    _fields: "id",
+  },
+  parseResponse: JSON.parse,
+})
+for (const p of resLife.value as Record<string, number>[]) {
+  postIds.push(p.id)
+}
+
+/**
+ * Prepare "game" category entries
+ */
+const { data: resGame } = await useFetch(`/wp/v2/posts`, {
+  baseURL: appConfig.baseURL,
+  params: {
+    page: 1,
+    per_page: 4,
+    type: "post",
+    subtype: "post",
+    status: ["publish"],
+    categories: [3],
+    _fields: "id",
+  },
+  parseResponse: JSON.parse,
+})
+for (const p of resGame.value as Record<string, number>[]) {
+  postIds.push(p.id)
+}
+
+/**
+ * Prepare "blog" category entries
+ */
+const { data: resBlog } = await useFetch(`/wp/v2/posts`, {
+  baseURL: appConfig.baseURL,
+  params: {
+    page: 1,
+    per_page: 4,
+    type: "post",
+    subtype: "post",
+    status: ["publish"],
+    categories: [548],
+    _fields: "id",
+  },
+  parseResponse: JSON.parse,
+})
+for (const p of resBlog.value as Record<string, number>[]) {
+  postIds.push(p.id)
+}
+
+/**
+ * Prepare "software-design" category entries
+ */
+const { data: resSoftwareDesign } = await useFetch(`/wp/v2/posts`, {
+  baseURL: appConfig.baseURL,
+  params: {
+    page: 1,
+    per_page: 4,
+    type: "post",
+    subtype: "post",
+    status: ["publish"],
+    categories: [1877],
+    _fields: "id",
+  },
+  parseResponse: JSON.parse,
+})
+for (const p of resSoftwareDesign.value as Record<string, number>[]) {
+  postIds.push(p.id)
+}
+
+/**
+ * Prepare "dreaming" category entries
+ */
+const { data: resDreaming } = await useFetch(`/wp/v2/posts`, {
+  baseURL: appConfig.baseURL,
+  params: {
+    page: 1,
+    per_page: 2,
+    type: "post",
+    subtype: "post",
+    status: ["publish"],
+    categories: [594],
+    _fields: "id",
+  },
+  parseResponse: JSON.parse,
+})
+for (const p of resDreaming.value as Record<string, number>[]) {
+  postIds.push(p.id)
+}
+
+/**
+ * Prepare "notes" category entries
+ */
+const { data: resNotes } = await useFetch(`/wp/v2/posts`, {
+  baseURL: appConfig.baseURL,
+  params: {
+    page: 1,
+    per_page: 2,
+    type: "post",
+    subtype: "post",
+    status: ["publish"],
+    categories: [1230],
+    _fields: "id",
+  },
+  parseResponse: JSON.parse,
+})
+for (const p of resNotes.value as Record<string, number>[]) {
+  postIds.push(p.id)
+}
+
+/**
+ * Merge them
+ */
+const { data: postSummaries } = await useFetch(`/mirumi/post_summaries_with_post_ids/${postIds.join(",")}`, {
+  baseURL: appConfig.baseURL,
+  parseResponse: JSON.parse,
+})
+posts = posts.concat(postSummaries.value as PageSummary[])
 
 useSetMeta({
   title: "みるめも",
@@ -24,6 +313,12 @@ useSetMeta({
 
 <style lang="scss" scoped>
 .index_view {
-
+  .row {
+    display: flex;
+    justify-content: space-between;
+    > * {
+      width: 45%;
+    }
+  }
 }
 </style>
