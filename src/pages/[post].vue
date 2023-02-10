d6a6f3596b0c753b277efd809823606bc08d8c72
mirumi <mail@mirumi.tech>
2023-02-10 18:25:04 +0900

    fix: how to load conten scripts

--- src/pages/[post].vue
+++ src/pages/[post].vue
@@ -94,8 +94,26 @@ const counts = {
   like: Number(post.like),
 }
 
-useHead({ script: [{ src: "/assets/prism.js", defer: true }] })
-useHead({ script: [{ src: "/assets/content-scripts.js", defer: true }] })
+// Content scripts
+onMounted(() => {
+  // Load YouTube Video iframe
+  const youtubes = document.querySelectorAll(".youtube") as NodeListOf<HTMLDivElement>
+
+  for (const target of youtubes) {
+    target.addEventListener("click", function () {
+      const iframe = document.createElement("iframe")
+      if (!target.dataset.video) return
+
+      iframe.src = target.dataset.video
+      iframe.style.width = "100%"
+      iframe.style.aspectRatio = "16/9"
+      iframe.style.margin = "0"
+      iframe.style.border = "solid 2.7px #dfd1c6"
+      iframe.style.borderRadius = "11px"
+      target.replaceWith(iframe)
+    })
+  }
+})
 
 const handleClick = (e: any) => {
   const link = e.target.closest("a")
@@ -120,6 +138,8 @@ const handleClick = (e: any) => {
   e.preventDefault()
   navigateTo(to)
 }
+
+useHead({ script: [{ src: "/assets/prism.js", defer: true }] })
 
 usePageInfo({
   title: post.title,
