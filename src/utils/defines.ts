d67fd60deeafde872b3a1c767ec2750454467f55
mirumi <mail@mirumi.me>
2026-08-08 22:17:25 +0900

    fix: remove category page meta data

--- src/utils/defines.ts
+++ src/utils/defines.ts
@@ -1,7 +1,7 @@
 export interface PageMeta {
   title: string
-  description: string
-  keywords: string
+  description?: string
+  keywords?: string
   thumbnail?: string
   url: string
   createdAt?: string
